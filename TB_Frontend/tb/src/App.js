a9f000f68f23f1366a81f831598fe6f8b3bea9d4
Sasi Koramutla <sasidhar.kmv@gmail.com>
2020-07-23 23:10:33 -0400

    updated delete route

--- TB_Frontend/tb/src/App.js
+++ TB_Frontend/tb/src/App.js
@@ -143,14 +143,15 @@
               }).catch(error =>  console.log({'Error': error}))
           }
 
-        delete = (event) => {
-         event.preventDefault();
+        delete = (index) => {
+         //event.preventDefault();
           console.log('this is the id: ', this.state.userid);
+          console.log('index: ', index);
           let loginInfo = JSON.parse(localStorage.getItem("loginInfo"));
               fetch(this.state.baseURL + '/tweetbattle/', {
                   method: 'DELETE',
                   body: JSON.stringify({ 
-                      index: event.target.id,
+                      index: index,
                       id: this.state.userid,
                       password:loginInfo.loginPassword,
                       token:loginInfo.token
@@ -344,7 +345,7 @@
                 this.state.savedTweets.map((tweet,index) => {
                   return (
                   <div className="w-25 p-3 savedTweet">
-                    <button style={{marginLeft:"80%", border:"none", backgroundColor:"white"}} onClick={this.delete} id={index}>
+                    <button style={{marginLeft:"80%", border:"none", backgroundColor:"white"}} onClick={e => this.delete(index)} key={index} value={index}>
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
