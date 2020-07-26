2ea68be0211b656fb91fadeca6a6989c532ea4e7
Sasi Koramutla <sasidhar.kmv@gmail.com>
2020-07-26 19:08:19 -0400

    updated env variables for heroku

---
+++ TB_back/public/build/precache-manifest.25dc0d147a6a29c596a62167063a164e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcab8e45dbc9df02f0ceabc4f85cfc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0ee3e256dfb22a7f257",
+    "url": "/static/css/main.17381dc5.chunk.css"
+  },
+  {
+    "revision": "1b0e4ec717abfb3f24b7",
+    "url": "/static/js/2.ca93eb3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca93eb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ee3e256dfb22a7f257",
+    "url": "/static/js/main.d82b30e7.chunk.js"
+  },
+  {
+    "revision": "593ad80a77fd8f09b05e",
+    "url": "/static/js/runtime-main.48781b86.js"
+  },
+  {
+    "revision": "04803081b74be24cd7eb79a6911b467e",
+    "url": "/static/media/KW.04803081.png"
+  },
+  {
+    "revision": "a799fd46d38a37d5ac1f7aecead75bbd",
+    "url": "/static/media/battleheader.a799fd46.jpg"
+  },
+  {
+    "revision": "44827db98f62d4db9a7d2cce855234fa",
+    "url": "/static/media/ezgif.com-crop (22).44827db9.gif"
+  },
+  {
+    "revision": "83453a5ca73752e1dbf7c4675f487201",
+    "url": "/static/media/trump-turd.83453a5c.png"
+  }
+]);
